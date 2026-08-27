caf5384accd8a1685658c2df00c56af34857e910
PranayBhumagouni283 <pbhumagouni@phenomiqs.com>
2026-08-28 00:22:07 +0530

    initial commit: NCT combined pipeline

---
+++ drop_trial_history.sql
@@ -0,0 +1,5 @@
+-- drop_trial_history.sql
+-- Run in Supabase SQL Editor to remove the trial_history table.
+-- nct_version_pairs replaces it with proper version pair comparisons.
+
+DROP TABLE IF EXISTS public.trial_history;
